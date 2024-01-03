5f0ac54fbce32a3a7f31badefe80daf379a672d5
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-03 22:49:40 +0530

    outputing question ,deriving values and register answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
 
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-    const [userAnswers,setUserAnswers] = useState([])
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
 
   return (
-<p>Currently active Question</p>  )
-}
+    <div className="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Quiz
+export default Quiz;
